0ea0ec83339cb8a5377fa44c5cfbb1c207256c0a
unknown <acterprime1@gmail.com>
2020-02-19 18:24:13 +0600

    ckeditor added

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -1,62 +1,22 @@
-@extends('layouts.layout')
+<!doctype html>
+<html lang="en">
 
+<head>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex, nofollow">
+  <title>Using Media Embed with Auto Embed</title>
+ 
+</head>
 
-@section('breadcrumb')
-@include('layouts.partials._hero_Index')
-@endsection
+<body>
+  <textarea class="form-control" id="summary-ckeditor" name="summary-ckeditor"></textarea>
+  <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+ <script>
+    CKEDITOR.replace( 'summary-ckeditor' );
+    CKEDITOR.config.height = 300;
+    CKEDITOR.config.width = 600;
+    CKEDITOR.config.extraPlugins = "Youtube";
+</script>
+</body>
 
-@section('content')
-<div style="padding: 100px">
-
-    Postrating    
-     @php
-        $halfstar = false;
-        if (!is_int($productRating)) {
-            $halfstar = true;
-            }
-      @endphp
-     <div class="rating product--rating">
-                 <ul>
-                   
-                     @for ($i = 0; $i < $productRating; $i++)
-                        <li>
-                            <span class="fa fa-star"></span>
-                        </li>
-                   @endfor
-                   @if ($halfstar)
-                        <li>
-                            <span class="fa fa-star-half-alt" style="color: #ffc000"></span>
-
-                        </li>
-                   @endif
-               </ul>
-              </div> 
-<ul>
-    @foreach ($comments as $comment)
-      <li>
-          <div>
-             {{ $comment->user->name }} {{$comment->user->getratings(1)}}
-             @php
-                 $rating = $comment->user->getratings(1)
-             @endphp
-              <div class="rating product--rating">
-                 <ul>
-                     @for ($i = 0; $i < $rating; $i++)
-                        <li>
-                            <span class="fa fa-star"></span>
-                        </li>
-                   @endfor
-               </ul>
-              </div>
-             <p>{{ $comment->body }}</p>
-          </div>
-        </li>
-
-        
-        @include('partials.replies', ['replies' => $comment->commentReplies])
-      
-    @endforeach
-</ul>
-<div style="padding: 100px">
-
-@endsection
+</html>
